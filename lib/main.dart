5714e2bb6b39219631c017a8143f34c1416dd183
ashup <ashupal0089@gmail.com>
2024-05-06 11:41:40 +0530

    6-5-24

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:adv_flutter_ch2/screens/date_picker/android/view/date_picker_scr
 import 'package:adv_flutter_ch2/screens/date_picker/ios/view/date_picker_ios.dart';
 import 'package:adv_flutter_ch2/screens/dialog_box/android/providers/android_dialog_provider.dart';
 import 'package:adv_flutter_ch2/screens/dialog_box/android/view/dialog_android.dart';
+import 'package:adv_flutter_ch2/screens/dialog_box/ios/view/action_sheet.dart';
 import 'package:adv_flutter_ch2/screens/time_picker/android/view/time_picker.dart';
 import 'package:adv_flutter_ch2/screens/time_picker/ios/providers/time_picker_ios_provider.dart';
 import 'package:adv_flutter_ch2/screens/time_picker/ios/view/time_picker_ios.dart';
@@ -48,13 +49,14 @@ class MyApp extends StatelessWidget {
             primarySwatch: Colors.lightGreen,
           ),
         ),
-        initialRoute: '/timePickerIos',
+        initialRoute: '/actionSheet',
         routes: {
           '/datePickerAndroid': (context) => DatePickerAndroid(),
           '/timePickerAndroid': (context) => TimePickerAndroid(),
           '/dialogBoxAndroid': (context) => DialogBoxAndroid(),
           '/datePickerIos': (context) => IosDatePicker(),
           '/timePickerIos': (context) => IosTimePicker(),
+          '/actionSheet': (context) => ActionSheet()
         },
       ),
     );
