df1d0bebc6dfa47e7b9995181e9b3d45369cb3fc
ashup <ashupal0089@gmail.com>
2024-05-09 10:09:16 +0530

    8-5-24

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:adv_flutter_ch2/screens/cupertino_slivers/cupertino_custom_scroll.dart';
+import 'package:adv_flutter_ch2/screens/cupertino_slivers/cupertino_list_section.dart';
 import 'package:adv_flutter_ch2/screens/date_picker/android/view/date_picker_screen.dart';
 import 'package:adv_flutter_ch2/screens/date_picker/ios/view/date_picker_ios.dart';
 import 'package:adv_flutter_ch2/screens/dialog_box/android/providers/android_dialog_provider.dart';
@@ -10,11 +12,12 @@ import 'package:adv_flutter_ch2/screens/material_cupertino_widgets/providers/swi
 import 'package:adv_flutter_ch2/screens/time_picker/android/view/time_picker.dart';
 import 'package:adv_flutter_ch2/screens/time_picker/ios/providers/time_picker_ios_provider.dart';
 import 'package:adv_flutter_ch2/screens/time_picker/ios/view/time_picker_ios.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp2());
 }
 
 class MyApp extends StatelessWidget {
@@ -22,48 +25,68 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(providers: [
-      ChangeNotifierProvider(
-        create: (context) => AndroidDialogProvider(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => AndroidDialogProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => IosTimeProvider(),
+        ),
+        ChangeNotifierProvider(create: (context) => SwitchProvider())
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+                datePickerTheme: DatePickerThemeData(
+                    backgroundColor: Color(0xffE5FBDA),
+                    dayForegroundColor: MaterialStateProperty.all(Colors.black),
+                    yearForegroundColor:
+                        MaterialStateProperty.all(Colors.white),
+                    todayBorder: BorderSide(
+                      color: Color(0xff396927),
+                    ),
+                    yearStyle: TextStyle(color: Colors.black),
+                    weekdayStyle: TextStyle(color: Color(0xff396927))),
+                timePickerTheme: TimePickerThemeData(
+                    dialHandColor: Color(0xff5f9e45),
+                    dayPeriodColor: Colors.lightBlue.withOpacity(0.5)))
+            .copyWith(
+          colorScheme: ColorScheme.fromSwatch(
+            brightness: Brightness.light,
+            primarySwatch: Colors.lightGreen,
+          ),
+        ),
+        initialRoute: '/customScroll',
+        routes: {
+          '/datePickerAndroid': (context) => DatePickerAndroid(),
+          '/timePickerAndroid': (context) => TimePickerAndroid(),
+          '/dialogBoxAndroid': (context) => DialogBoxAndroid(),
+          '/datePickerIos': (context) => IosDatePicker(),
+          '/timePickerIos': (context) => IosTimePicker(),
+          '/actionSheet': (context) => ActionSheet(),
+          '/materialWidgets': (context) => MaterialWidgets(),
+          '/cupertinoWidgets': (context) => CupertinoWidgets(),
+          '/adaptive': (context) => AdaptiveMaterialCupertino(),
+          '/customScroll': (context) => CustomScroll(),
+        },
       ),
-      ChangeNotifierProvider(
-        create: (context) => IosTimeProvider(),
-      ),
-      ChangeNotifierProvider(create: (context) => SwitchProvider())
-    ], child: AdaptiveMaterialCupertino());
+    );
   }
 }
-// MaterialApp(
-// debugShowCheckedModeBanner: false,
-// theme: ThemeData(
-// datePickerTheme: DatePickerThemeData(
-// backgroundColor: Color(0xffE5FBDA),
-// dayForegroundColor: MaterialStateProperty.all(Colors.black),
-// yearForegroundColor:
-// MaterialStateProperty.all(Colors.white),
-// todayBorder: BorderSide(
-// color: Color(0xff396927),
-// ),
-// yearStyle: TextStyle(color: Colors.black),
-// weekdayStyle: TextStyle(color: Color(0xff396927))),
-// timePickerTheme: TimePickerThemeData(
-// dialHandColor: Color(0xff5f9e45),
-// dayPeriodColor: Colors.lightBlue.withOpacity(0.5)))
-//     .copyWith(
-// colorScheme: ColorScheme.fromSwatch(
-// brightness: Brightness.light,
-// primarySwatch: Colors.lightGreen,
-// ),
-// ),
-// initialRoute: '/cupertinoWidgets',
-// routes: {
-// '/datePickerAndroid': (context) => DatePickerAndroid(),
-// '/timePickerAndroid': (context) => TimePickerAndroid(),
-// '/dialogBoxAndroid': (context) => DialogBoxAndroid(),
-// '/datePickerIos': (context) => IosDatePicker(),
-// '/timePickerIos': (context) => IosTimePicker(),
-// '/actionSheet': (context) => ActionSheet(),
-// '/materialWidgets': (context) => MaterialWidgets(),
-// '/cupertinoWidgets': (context) => CupertinoWidgets(),
-// },
-// ),
+
+class MyApp2 extends StatelessWidget {
+  const MyApp2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/listSection',
+      routes: {
+        '/': (context) => CustomScroll(),
+        '/listSection': (p0) => CupertinoListSectionExample(),
+      },
+    );
+  }
+}
---
+++ lib/screens/cupertino_slivers/cupertino_list_section.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+
+import 'components/multi_selection_list_section.dart';
+import 'components/single_selection_list_section.dart';
+
+class CupertinoListSectionExample extends StatelessWidget {
+  const CupertinoListSectionExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      backgroundColor: CupertinoColors.systemGrey6,
+      navigationBar: CupertinoNavigationBar(
+        previousPageTitle: "Back",
+        middle: Text("Cupertino List Enhanced"),
+      ),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 68,
+          ),
+          SingleSelectionListSelection(),
+          MultiSelectionListSection(),
+        ],
+      ),
+    );
+  }
+}
